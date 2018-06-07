efb235369033769287db9faf3456c1b22de15304
saileshkalakoti <kalakotisaileshh@gmail.com>
2018-06-07 18:07:24 +0530

    component created

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Rectangle from './App';
+
+ReactDOM.render(<Rectangle />, document.getElementById('root'));
+
