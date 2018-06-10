f35f739362f0493cf90d2447cb77616da974415a
suraj16101 <suraj16101@iiitd.ac.in>
2018-06-10 23:34:03 +0530

    Mergering

--- Header/src/index.js
+++ Header/src/index.js
@@ -3,21 +3,32 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import './index.css';
 import bg from './background.jpg'
+import Rectangle from './App';
+import Search from './Search';
 class Woc_header extends React.Component
 {
     render()
     {
     return (
         <div className="wall">
-        <div className="Rectangle-3">
-        <div className="Bitmap">
-            <div className="Wall-of-Contribution" >
-            WALL OF CONTRIBUTION
-            
+            <div className="Rectangle-3">
+
+                <div className="Bitmap">
+                    <div className="Wall-of-Contribution" >
+                        WALL OF CONTRIBUTION
+                    </div>
+                    <div className="Coming-together-is-a">
+                        Coming together is a beginning. Keeping together is progress. Working together is success.
+                    </div>
+                </div>
+                <div>
+                    <Search />
+                </div>
+
+                <div className="rect">
+                     <Rectangle />
+                </div>
             </div>
-            <div className="Coming-together-is-a">Coming together is a beginning. Keeping together is progress. Working together is success.</div>
-        </div>
-        </div>
         </div>
         
 
